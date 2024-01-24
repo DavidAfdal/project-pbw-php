a9b2cb6c22b96e5c7a4a1856f0a37952c1caddb0
DiasHafizhan <diashafizhan32@gmail.com>
2024-01-24 13:34:32 +0700

    ppp

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php include("./partials/head.php"); ?>
+</head>
+<body>
+<div class="background">
+      <div class="d-flex justify-content-between">
+        <h1>Tentang kami</h1>
+        <ul>
+          <li><a href="">Login</a></li>
+          <li><a href="">Daftar</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="info">
+      <a class="">
+        <i class="bx bx-info-circle"></i>
+      </a>
+    </div>
+
+    <div class="panduan" id="panduan">
+      <div class="">
+        <h3>Panduan Cara Konsisrten</h3>
+
+        <div class="pdf d-flex">
+          <a href="">
+            <i class="bx bx-file"></i>
+            Panduan cara merenung.pdf
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+    <script src="./app.js"></script>
+  </body>
+</body>
+</html>
